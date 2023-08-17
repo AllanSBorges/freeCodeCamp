0888392906a44fc2911aace7427a761f27161766
Allan da Silva Borges <120101127+AllanSBorges@users.noreply.github.com>
2023-08-17 18:45:13 -0300

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -14,31 +14,31 @@ cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPON
 do
   if [[ $YEAR != 'year' ]]
   then
-    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE team = '$WINNER'")
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
 
     if [[ -z $WINNER_ID ]]
     then
-      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(team) VALUES('$WINNER')")
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
       if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
       then
         echo Inserted into team, $WINNER
       fi
 
-      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE team = '$WINNER'")
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
     fi
 
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE team = '$OPPONENT'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
 
     if [[ -z $OPPONENT_ID ]]
     then
-      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(team) VALUES ('$OPPONENT')")
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
       if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
       then
         echo Inserted into team, $OPPONENT
 
       fi
 
-      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE team = '$OPPONENT'")
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
     fi
     
     INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS)")
